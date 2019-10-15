fb7b072df6f4395edf23d951214fff34e0b83af6
School of Information Student <student@ischool.rit.edu>
2019-10-14 20:59:35 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Student on 10/14/19.
+//  Copyright © 2019 Regina Bass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
