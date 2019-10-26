6e32292ad8c477cd2142485f116d6dc7700dc69e
Regina Bass <reginabassvdl@gmail.com>
2019-10-25 23:52:29 -0400

    SuperHero 'Show' Navigation complete - Similar Movie Reccommendations

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Regina on 10/25/19.
+//  Copyright © 2019 Regina Bass. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
